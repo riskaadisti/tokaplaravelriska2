1293891c98ff3f2493ed78e28f89a283bf11faa6
riskaadisti <riskaariski@gmail.com>
2019-02-17 14:00:12 +0700

    eps 15

--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -43,7 +43,8 @@
 		<td>{{ $dt->email }}</td>
 		<td>{{ $dt->akses }}</td>
 		<td>
-			<a href="#" class="btn btn-success btn-5m">
+			<a href="{{route('admin.user.edit',['id'=>$dt->id])}}"
+			class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
 
